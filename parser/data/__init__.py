b5888bbc3dfade859a5cd026644c1d1122c579ba
yzhangcs <yzhang.cs@outlook.com>
2019-03-09 10:20:59 +0800

    Add data preprocessing modules

---
+++ parser/data/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .dataset import TextDataset, collate_fn
+from .reader import Corpus, Embedding
+from .vocab import Vocab
+
+__all__ = ('Corpus', 'Embedding', 'TextDataset', 'Vocab', 'collate_fn')
