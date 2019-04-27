7d02ec9b50fd55815b6a5749c2e385160e93fb4d
zysite <zysite@outlook.com>
2019-04-27 16:49:12 +0800

    Run the program by subcommands

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from parser.parser import BiaffineParser
-from parser.trainer import Trainer
 
-__all__ = ('BiaffineParser', 'Trainer')
+
+__all__ = ('BiaffineParser')
---
+++ parser/cmds/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .evaluate import Evaluate
+from .predict import Predict
+from .train import Train
+
+
+__all__ = ('Evaluate', 'Predict', 'Train')
