6c954203eaa7ea39250d40ff1c4cb550d8888782
zysite <zysite@outlook.com>
2019-04-27 17:01:22 +0800

    Replace char_lstm with tag embedding

--- parser/modules/__init__.py
+++ parser/modules/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from .biaffine import Biaffine
-from .char_lstm import CHAR_LSTM
 from .lstm import LSTM
 from .mlp import MLP
 
 
-__all__ = ['CHAR_LSTM', 'LSTM', 'MLP', 'Biaffine']
+__all__ = ['LSTM', 'MLP', 'Biaffine']
