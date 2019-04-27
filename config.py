bb26ebaf5226ea8436ad38893d3c5d9f05fa952d
zysite <zysite@outlook.com>
2019-04-27 16:53:33 +0800

    Hyperparam configurations

--- config.py
+++ config.py
@@ -2,12 +2,28 @@
 
 
 class Config(object):
+
+    # [Network]
     n_embed = 100
     n_char_embed = 50
     n_char_out = 100
+    embed_dropout = 0.33
     n_lstm_hidden = 400
     n_lstm_layers = 3
+    lstm_dropout = 0.33
     n_mlp_arc = 500
     n_mlp_lab = 100
-    betas = (0.9, 0.9)
+    mlp_dropout = 0.33
+
+    # [Optimizer]
+    lr = 2e-3
+    beta_1 = 0.9
+    beta_2 = 0.9
     epsilon = 1e-12
+    decay = .75
+    decay_steps = 5000
+
+    # [Run]
+    batch_size = 200
+    epochs = 1000
+    patience = 100
