d22e0a4cb488d1818656eb4f25270b63e4e19c86
zysite <zysite@outlook.com>
2019-04-27 17:00:32 +0800

    Extract common options from subcommands

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
 from parser.cmds import Evaluate, Predict, Train
 
+import torch
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -16,5 +18,23 @@ if __name__ == '__main__':
     }
     for name, subcommand in subcommands.items():
         subparser = subcommand.add_subparser(name, subparsers)
+        subparser.add_argument('--device', '-d', default='-1',
+                               help='ID of GPU to use')
+        subparser.add_argument('--seed', '-s', default=1, type=int,
+                               help='seed for generating random numbers')
+        subparser.add_argument('--threads', '-t', default=4, type=int,
+                               help='max num of threads')
+        subparser.add_argument('--file', '-f', default='model.pt',
+                               help='path to model file')
+        subparser.add_argument('--vocab', '-v', default='vocab.pt',
+                               help='path to vocabulary file')
     args = parser.parse_args()
+
+    print(f"Set the max num of threads to {args.threads}")
+    print(f"Set the seed for generating random numbers to {args.seed}")
+    print(f"Set the device with ID {args.device} visible")
+    torch.set_num_threads(args.threads)
+    torch.manual_seed(args.seed)
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.device
+
     args.func(args)
