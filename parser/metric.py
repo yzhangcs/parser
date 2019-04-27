b655b15d787570d8ad35e96b79e8f4c051b4f3b5
yzhangcs <yzhang.cs@outlook.com>
2019-04-27 16:51:40 +0800

    Trivial modifications

--- parser/metric.py
+++ parser/metric.py
@@ -33,36 +33,30 @@ class AttachmentMethod(Metric):
     def __init__(self, eps=1e-5):
         super(AttachmentMethod, self).__init__()
 
+        self.eps = eps
+        self.total = 0.0
         self.correct_arcs = 0.0
         self.correct_labels = 0.0
-        self.total = 0.0
-        self.eps = eps
 
-    def __call__(self, s_arc, s_lab, heads, labels, mask):
-        s_arc = s_arc[mask]
-        s_lab = s_lab[mask]
-        heads = heads[mask]
-        labels = labels[mask]
-        s_lab = s_lab[torch.arange(len(s_arc)), heads]
+    def __call__(self, pred_heads, pred_labels, gold_heads, gold_labels):
+        arc_mask = pred_heads.eq(gold_heads)
+        lab_mask = pred_labels.eq(gold_labels) & arc_mask
 
-        arc_mask = (torch.argmax(s_arc, dim=1) == heads)
-        lab_mask = (torch.argmax(s_lab, dim=1) == labels)
-
-        self.total += len(s_arc)
+        self.total += len(arc_mask)
         self.correct_arcs += arc_mask.sum().item()
-        self.correct_labels += (arc_mask * lab_mask).sum().item()
+        self.correct_labels += lab_mask.sum().item()
 
     def __repr__(self):
-        return f"LAS: {self.las:.2%} UAS: {self.uas:.2%}"
+        return f"UAS: {self.uas:.2%} LAS: {self.las:.2%}"
 
     @property
     def score(self):
         return self.las
 
     @property
-    def las(self):
-        return self.correct_labels / (self.total + self.eps)
-
-    @property
     def uas(self):
         return self.correct_arcs / (self.total + self.eps)
+
+    @property
+    def las(self):
+        return self.correct_labels / (self.total + self.eps)
--- parser/modules/__init__.py
+++ parser/modules/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from .biaffine import Biaffine
-from .char_lstm import CharLSTM
+from .char_lstm import CHAR_LSTM
 from .mlp import MLP
 from .parser_lstm import ParserLSTM
 
 
-__all__ = ('Biaffine', 'CharLSTM', 'MLP', 'ParserLSTM')
+__all__ = ('Biaffine', 'CHAR_LSTM', 'MLP', 'ParserLSTM')
