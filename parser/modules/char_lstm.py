dd484875b2a8fe6e42984bc7a14f7017ebd3f616
zysite <zysite@outlook.com>
2019-04-27 16:51:40 +0800

    Trivial modifications

--- parser/modules/char_lstm.py
+++ parser/modules/char_lstm.py
@@ -5,29 +5,29 @@ import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence
 
 
-class CharLSTM(nn.Module):
+class CHAR_LSTM(nn.Module):
 
     def __init__(self, n_char, n_embed, n_out):
-        super(CharLSTM, self).__init__()
+        super(CHAR_LSTM, self).__init__()
 
         # the embedding layer
         self.embed = nn.Embedding(num_embeddings=n_char,
                                   embedding_dim=n_embed)
         # the lstm layer
         self.lstm = nn.LSTM(input_size=n_embed,
-                            hidden_size=n_out // 2,
+                            hidden_size=n_out//2,
                             batch_first=True,
                             bidirectional=True)
 
     def forward(self, x):
         mask = x.gt(0)
-        lens, indices = torch.sort(mask.sum(dim=1), descending=True)
-        _, inverse_indices = indices.sort()
-        max_len = lens[0]
+        sorted_lens, indices = torch.sort(mask.sum(dim=1), descending=True)
+        inverse_indices = indices.argsort()
+        max_len = sorted_lens[0]
         x = x[indices, :max_len]
         x = self.embed(x)
 
-        x = pack_padded_sequence(x, lens, True)
+        x = pack_padded_sequence(x, sorted_lens, True)
         x, (hidden, _) = self.lstm(x)
         reprs = torch.cat(torch.unbind(hidden), dim=1)
         reprs = reprs[inverse_indices]
