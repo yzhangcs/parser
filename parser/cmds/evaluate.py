80f94e38589894f36c4ec8d8c9c1d177e52db057
yzhangcs <yzhang.cs@outlook.com>
2019-05-02 20:42:32 +0800

    Divide sentences into several buckets by length

--- parser/cmds/evaluate.py
+++ parser/cmds/evaluate.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from parser import BiaffineParser, Model
-from parser.utils import Corpus, TextDataset, collate_fn
+from parser.utils import Corpus
+from parser.utils.data import TextDataset, batchify
 
 import torch
-from torch.utils.data import DataLoader
 
 
 class Evaluate(object):
@@ -13,30 +13,29 @@ class Evaluate(object):
         subparser = parser.add_parser(
             name, help='Evaluate the specified model and dataset.'
         )
-        subparser.add_argument('--batch-size', default=200, type=int,
+        subparser.add_argument('--batch-size', default=5000, type=int,
                                help='batch size')
-        subparser.add_argument('--include-punct', action='store_true',
+        subparser.add_argument('--buckets', default=64, type=int,
+                               help='max num of buckets to use')
+        subparser.add_argument('--punct', action='store_true',
                                help='whether to include punctuation')
-        subparser.add_argument('--fdata', default='data/test.conllx',
+        subparser.add_argument('--fdata', default='data/ptb/test.conllx',
                                help='path to dataset')
-        subparser.set_defaults(func=self)
 
         return subparser
 
-    def __call__(self, args):
+    def __call__(self, config):
         print("Load the model")
-        vocab = torch.load(args.vocab)
-        network = BiaffineParser.load(args.file)
-        model = Model(vocab, network)
+        vocab = torch.load(config.vocab)
+        parser = BiaffineParser.load(config.model)
+        model = Model(vocab, parser)
 
         print("Load the dataset")
-        corpus = Corpus.load(args.fdata)
+        corpus = Corpus.load(config.fdata)
         dataset = TextDataset(vocab.numericalize(corpus))
         # set the data loader
-        loader = DataLoader(dataset=dataset,
-                            batch_size=args.batch_size,
-                            collate_fn=collate_fn)
+        loader = batchify(dataset, config.batch_size, config.buckets)
 
         print("Evaluate the dataset")
-        loss, metric = model.evaluate(loader, include_punct=args.include_punct)
+        loss, metric = model.evaluate(loader, config.punct)
         print(f"Loss: {loss:.4f} {metric}")
