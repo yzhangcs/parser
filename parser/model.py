d27977e8d206da8c7cdc56620824042f5bbf9464
yzhangcs <yzhang.cs@outlook.com>
2020-05-30 00:39:17 +0800

    Remove unused modules

--- parser/model.py
+++ parser/model.py
@@ -7,8 +7,7 @@ from parser.utils.fn import istree
 
 import torch
 import torch.nn as nn
-from torch.nn.utils.rnn import (pack_padded_sequence, pad_packed_sequence,
-                                pad_sequence)
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class Model(nn.Module):
