c63bf759b2f171d1765384c7da236056613b353f
yzhangcs <yzhang.cs@outlook.com>
2020-06-13 19:13:19 +0800

    ChuLiu/Edmod Algorithm

--- supar/utils/alg.py
+++ supar/utils/alg.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from supar.utils.fn import istree
 import torch
 from supar.utils.fn import pad, stripe
 
@@ -42,47 +43,6 @@ def kmeans(x, k):
     clusters = [torch.where(y.eq(i))[0].tolist() for i in assigned]
 
     return centroids, clusters
-
-
-def tarjan(sequence):
-    sequence[0] = -1
-    # record the search order, i.e., the timestep
-    dfn = [-1] * len(sequence)
-    # record the the smallest timestep in a SCC
-    low = [-1] * len(sequence)
-    # push the visited into the stack
-    stack, onstack = [], [False] * len(sequence)
-
-    def connect(i, timestep):
-        dfn[i] = low[i] = timestep[0]
-        timestep[0] += 1
-        stack.append(i)
-        onstack[i] = True
-
-        for j, head in enumerate(sequence):
-            if head != i:
-                continue
-            if dfn[j] == -1:
-                yield from connect(j, timestep)
-                low[i] = min(low[i], low[j])
-            elif onstack[j]:
-                low[i] = min(low[i], dfn[j])
-
-        # a SCC is completed
-        if low[i] == dfn[i]:
-            cycle = [stack.pop()]
-            while cycle[-1] != i:
-                onstack[cycle[-1]] = False
-                cycle.append(stack.pop())
-            onstack[i] = False
-            # ignore the self-loop
-            if len(cycle) > 1:
-                yield cycle
-
-    timestep = [0]
-    for i in range(len(sequence)):
-        if dfn[i] == -1:
-            yield from connect(i, timestep)
 
 
 def eisner(scores, mask):
@@ -183,3 +143,144 @@ def cky(scores, mask):
              for i, length in enumerate(lens.tolist())]
 
     return trees
+
+
+def tarjan(sequence):
+    sequence[0] = -1
+    # record the search order, i.e., the timestep
+    dfn = [-1] * len(sequence)
+    # record the the smallest timestep in a SCC
+    low = [-1] * len(sequence)
+    # push the visited into the stack
+    stack, onstack = [], [False] * len(sequence)
+
+    def connect(i, timestep):
+        dfn[i] = low[i] = timestep[0]
+        timestep[0] += 1
+        stack.append(i)
+        onstack[i] = True
+
+        for j, head in enumerate(sequence):
+            if head != i:
+                continue
+            if dfn[j] == -1:
+                yield from connect(j, timestep)
+                low[i] = min(low[i], low[j])
+            elif onstack[j]:
+                low[i] = min(low[i], dfn[j])
+
+        # a SCC is completed
+        if low[i] == dfn[i]:
+            cycle = [stack.pop()]
+            while cycle[-1] != i:
+                onstack[cycle[-1]] = False
+                cycle.append(stack.pop())
+            onstack[i] = False
+
+            # ignore the self-loop
+            if len(cycle) > 1:
+                yield cycle
+
+    timestep = [0]
+    for i in range(len(sequence)):
+        if dfn[i] == -1:
+            yield from connect(i, timestep)
+
+
+def chuliu_edmonds(s):
+    tree = s.argmax(-1)
+    # return the cycle finded by tarjan algorithm lazily
+    cycle = next(tarjan(tree.tolist()), None)
+    # if `tree` has no cycles, then it is a MST
+    if not cycle:
+        return tree
+    # indices of cycle in the original tree
+    cycle = torch.tensor(cycle)
+    # indices of noncycle in the original tree
+    noncycle = torch.ones(len(s)).index_fill_(0, cycle, 0)
+    noncycle = torch.where(noncycle.gt(0))[0]
+
+    def contract(s):
+        # heads of cycle in original tree
+        cycle_heads = tree[cycle]
+        # scores of cycle in original tree
+        s_cycle = s[cycle, cycle_heads]
+
+        # calculate the scores of cycle's potential dependents
+        # s(c->x) = max(s(x'->x)), x in noncycle and x' in cycle
+        s_dep = s[noncycle][:, cycle]
+        # find the best cycle head for each noncycle dependent
+        deps = s_dep.argmax(1)
+        # calculate the scores of cycle's potential heads
+        # s(x->c) = max(s(x'->x) - s(a(x')->x') + s(cycle)),
+        #           x in noncycle and x' in cycle
+        #           a(v) is the predecessor of v in cycle
+        #           s(cycle) = sum(s(a(v)->v))
+        s_head = s[cycle][:, noncycle] - s_cycle.view(-1, 1) + s_cycle.sum()
+        # find the best noncycle head for each cycle dependent
+        heads = s_head.argmax(0)
+
+        contracted = torch.cat((noncycle, torch.tensor([-1])))
+        # calculate the scores of contracted graph
+        s = s[contracted][:, contracted]
+        # set the contracted graph scores of cycle's potential dependents
+        s[:-1, -1] = s_dep[range(len(deps)), deps]
+        # set the contracted graph scores of cycle's potential heads
+        s[-1, :-1] = s_head[heads, range(len(heads))]
+
+        return s, heads, deps
+
+    # keep track of the endpoints of the edges into and out of cycle
+    # for reconstruction later
+    s, heads, deps = contract(s)
+
+    # y is the contracted tree
+    y = chuliu_edmonds(s)
+    # exclude head of cycle from y
+    y, cycle_head = y[:-1], y[-1]
+
+    # fix the subtree with no heads coming from the cycle
+    # len(y) denotes heads coming from the cycle
+    subtree = y < len(y)
+    # add the nodes to the new tree
+    tree[noncycle[subtree]] = noncycle[y[subtree]]
+    # fix the subtree with heads coming from the cycle
+    subtree = ~subtree
+    # add the nodes to the tree
+    tree[noncycle[subtree]] = cycle[deps[subtree]]
+    # fix the root of the cycle
+    cycle_root = heads[cycle_head]
+    # break the cycle and add the root of the cycle to the tree
+    tree[cycle[cycle_root]] = noncycle[cycle_head]
+
+    return tree
+
+
+def mst(scores, mask, multiroot=False):
+    r'''
+    Please note that the ChuLiu/Edmod algorithm does not guarantee
+    to parse a tree with single root.
+    '''
+    batch_size, seq_len, _ = scores.shape
+    scores = scores.cpu().unbind()
+
+    preds = []
+    for i, length in enumerate(mask.sum(1).tolist()):
+        s = scores[i][:length+1, :length+1]
+        tree = chuliu_edmonds(s)
+        roots = torch.where(tree[1:].eq(0))[0] + 1
+        if not multiroot and len(roots) > 1:
+            s_root = s[:, 0]
+            s_best = float('-inf')
+            s = s.index_fill(1, torch.tensor(0), float('-inf'))
+            for root in roots:
+                s[:, 0] = float('-inf')
+                s[root, 0] = s_root[root]
+                t = chuliu_edmonds(s)
+                s_tree = s[1:].gather(1, t[1:].unsqueeze(-1)).sum()
+                if s_tree > s_best:
+                    s_best, tree = s_tree, t
+        assert istree(tree.tolist())
+        preds.append(tree)
+
+    return pad(preds, total_length=seq_len).to(mask.device)
