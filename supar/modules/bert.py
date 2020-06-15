9cc4cda308218675646c72f572514ec660856872
yzhangcs <yzhang.cs@outlook.com>
2020-06-16 00:41:13 +0800

    Fix bug of n_layers

--- supar/modules/bert.py
+++ supar/modules/bert.py
@@ -23,9 +23,9 @@ class BertEmbedding(nn.Module):
         config = AutoConfig.from_pretrained(model, output_hidden_states=True)
         self.bert = AutoModel.from_config(config)
         self.bert = self.bert.requires_grad_(requires_grad)
-        self.n_layers = n_layers
+        self.n_layers = n_layers or self.bert.config.num_hidden_layers
         self.hidden_size = self.bert.config.hidden_size
-        self.n_out = n_out if n_out != 0 else self.hidden_size
+        self.n_out = n_out or self.hidden_size
         self.pad_index = pad_index
         self.requires_grad = requires_grad
 
@@ -35,9 +35,8 @@ class BertEmbedding(nn.Module):
 
     def __repr__(self):
         s = self.__class__.__name__ + '('
-        if self.n_layers != 0:
-            s += f"n_layers={self.n_layers}, "
-        s += f"n_out={self.n_out}, pad_index={self.pad_index}"
+        s += f"n_layers={self.n_layers}, n_out={self.n_out}, "
+        s += f"pad_index={self.pad_index}"
         if self.requires_grad:
             s += f", requires_grad={self.requires_grad}"
         s += ')'
