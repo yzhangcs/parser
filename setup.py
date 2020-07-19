5d2c71ea1f9e1f334a02a66c3f996571fdf76915
yzhangcs <yzhang.cs@outlook.com>
2020-07-19 17:10:25 +0800

    Requires nltk

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="supar",
-    version="0.1.0",
-    author="Yu Zhang",
-    author_email="yzhang.cs@outlook.com",
-    description="Syntactic Parsing Models",
-    long_description=open("README.md", "r").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/yzhangcs/parser",
+    name='supar',
+    version='0.1.0',
+    author='Yu Zhang',
+    author_email='yzhang.cs@outlook.com',
+    description='Syntactic Parsing Models',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yzhangcs/parser',
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Text Processing :: Linguistic"
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Text Processing :: Linguistic'
     ],
     setup_requires=[
         'setuptools>=18.0',
     ],
-    install_requires=["torch>=1.4.0", "transformers"],
+    install_requires=['torch>=1.4.0', 'transformers', 'nltk'],
     entry_points={
         'console_scripts': [
             'biaffine-dependency=supar.cmds.biaffine_dependency:main',
