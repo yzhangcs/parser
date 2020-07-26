3c0932fb55f5452d26e5e7adbcbf0a4f1949c117
Yu Zhang <yzhang.cs@outlook.com>
2020-07-26 21:40:10 +0800

    sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,6 @@
 
 import supar
 
-import stanford_theme
-
 # -- Project information -----------------------------------------------------
 
 project = 'SuPar'
@@ -74,8 +72,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'stanford_theme'
-html_theme_path = [stanford_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
