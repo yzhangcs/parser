7f36b10f71b4b677b466847cb6ac41d257828110
yzhangcs <yzhang.cs@outlook.com>
2021-04-27 00:50:47 +0800

    Support parsing unregistered args

--- supar/cmds/cmd.py
+++ supar/cmds/cmd.py
@@ -7,17 +7,16 @@ from supar.utils.parallel import init_device
 
 
 def parse(parser):
-    parser.add_argument('--conf', '-c', help='path to config file')
     parser.add_argument('--path', '-p', help='path to model file')
+    parser.add_argument('--conf', '-c', default='', help='path to config file')
     parser.add_argument('--device', '-d', default='-1', help='ID of GPU to use')
     parser.add_argument('--seed', '-s', default=1, type=int, help='seed for generating random numbers')
     parser.add_argument('--threads', '-t', default=16, type=int, help='max num of threads')
     parser.add_argument('--batch-size', default=5000, type=int, help='batch size')
-    parser.add_argument('--local_rank', default=-1, type=int, help='node rank for distributed training')
-    parser.add_argument('--epochs', default=5000, type=int, help='epochs')
+    parser.add_argument("--local_rank", type=int, default=-1, help='node rank for distributed training')
     args, unknown = parser.parse_known_args()
-    args, _ = parser.parse_known_args(unknown, args)
-    args = Config(**vars(args))
+    args, unknown = parser.parse_known_args(unknown, args)
+    args = Config.load(**vars(args), unknown=unknown)
     Parser = args.pop('Parser')
 
     torch.set_num_threads(args.threads)
