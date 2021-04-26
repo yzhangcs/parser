8944f2abb34fb3ce46dd07bf7b78ef3a287c87bd
yzhangcs <yzhang.cs@outlook.com>
2021-04-27 00:38:54 +0800

    Reorganize & Rename modules

--- supar/modules/transformer.py
+++ supar/modules/transformer.py
@@ -3,44 +3,44 @@
 import torch
 import torch.nn as nn
 from supar.modules.scalar_mix import ScalarMix
-from torch.nn.utils.rnn import pad_sequence
+from supar.utils.fn import pad
 
 
-class BertEmbedding(nn.Module):
+class TransformerEmbedding(nn.Module):
     r"""
     A module that directly utilizes the pretrained models in `transformers`_ to produce BERT representations.
     While mainly tailored to provide input preparation and post-processing for the BERT model,
-    it is also compatiable with other pretrained language models like XLNet, RoBERTa and ELECTRA, etc.
+    it is also compatible with other pretrained language models like XLNet, RoBERTa and ELECTRA, etc.
 
     Args:
         model (str):
             Path or name of the pretrained models registered in `transformers`_, e.g., ``'bert-base-cased'``.
         n_layers (int):
-            The number of layers from the model to use.
-            If 0, uses all layers.
+            The number of BERT layers to use. If 0, uses all layers.
         n_out (int):
-            The requested size of the embeddings.
-            If 0, uses the size of the pretrained embedding model.
+            The requested size of the embeddings. If 0, uses the size of the pretrained embedding model. Default: 0.
         stride (int):
-            A sequence longer than the limited max length will be splitted into several small pieces
-            with a window size of ``stride``. Default: 5.
+            A sequence longer than max length will be splitted into several small pieces
+            with a window size of ``stride``. Default: 10.
+        pooling (str):
+            Pooling way to get from token piece embeddings to token embedding.
+            ``first``: take the first subtoken. ``last``: take the last subtoken. ``mean``: take a mean over all.
+            Default: ``mean``.
         pad_index (int):
-            The index of the padding token in the BERT vocabulary. Default: 0.
+            The index of the padding token in BERT vocabulary. Default: 0.
         dropout (float):
-            The dropout ratio of BERT layers. Default: 0.
-            This value will be passed into the :class:`ScalarMix` layer.
+            The dropout ratio of BERT layers. Default: 0. This value will be passed into the :class:`ScalarMix` layer.
         requires_grad (bool):
-            If ``True``, the model parameters will be updated together with the downstream task.
-            Default: ``False``.
+            If ``True``, the model parameters will be updated together with the downstream task. Default: ``False``.
 
     .. _transformers:
         https://github.com/huggingface/transformers
     """
 
-    def __init__(self, model, n_layers, n_out, stride=5, pad_index=0, dropout=0, requires_grad=False):
+    def __init__(self, model, n_layers, n_out=0, stride=256, pooling='mean', pad_index=0, dropout=0, requires_grad=False):
         super().__init__()
 
-        from transformers import AutoConfig, AutoModel
+        from transformers import AutoConfig, AutoModel, AutoTokenizer
         self.bert = AutoModel.from_pretrained(model, config=AutoConfig.from_pretrained(model, output_hidden_states=True))
         self.bert = self.bert.requires_grad_(requires_grad)
 
@@ -49,16 +49,20 @@ class BertEmbedding(nn.Module):
         self.hidden_size = self.bert.config.hidden_size
         self.n_out = n_out or self.hidden_size
         self.stride = stride
+        self.pooling = pooling
         self.pad_index = pad_index
         self.dropout = dropout
         self.requires_grad = requires_grad
-        self.max_len = self.bert.config.max_position_embeddings
+        self.max_len = int(max(0, self.bert.config.max_position_embeddings) or 1e12) - 2
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
 
         self.scalar_mix = ScalarMix(self.n_layers, dropout)
         self.projection = nn.Linear(self.hidden_size, self.n_out, False) if self.hidden_size != n_out else nn.Identity()
 
     def __repr__(self):
-        s = f"{self.model}, n_layers={self.n_layers}, n_out={self.n_out}, pad_index={self.pad_index}"
+        s = f"{self.model}, n_layers={self.n_layers}, n_out={self.n_out}, "
+        s += f"stride={self.stride}, pooling={self.pooling}, pad_index={self.pad_index}"
         if self.dropout > 0:
             s += f", dropout={self.dropout}"
         if self.requires_grad:
@@ -74,12 +78,12 @@ class BertEmbedding(nn.Module):
             ~torch.Tensor:
                 BERT embeddings of shape ``[batch_size, seq_len, n_out]``.
         """
-        batch_size, seq_len, fix_len = subwords.shape
+
         mask = subwords.ne(self.pad_index)
         lens = mask.sum((1, 2))
         # [batch_size, n_subwords]
-        subwords = pad_sequence(subwords[mask].split(lens.tolist()), True)
-        bert_mask = pad_sequence(mask[mask].split(lens.tolist()), True)
+        subwords = pad(subwords[mask].split(lens.tolist()), self.pad_index, padding_side=self.tokenizer.padding_side)
+        bert_mask = pad(mask[mask].split(lens.tolist()), 0, padding_side=self.tokenizer.padding_side)
 
         # return the hidden states of all layers
         bert = self.bert(subwords[:, :self.max_len], attention_mask=bert_mask[:, :self.max_len].float())[-1]
@@ -92,14 +96,18 @@ class BertEmbedding(nn.Module):
             part = self.bert(subwords[:, i:i+self.max_len], attention_mask=bert_mask[:, i:i+self.max_len].float())[-1]
             bert = torch.cat((bert, self.scalar_mix(part[-self.n_layers:])[:, self.max_len-self.stride:]), 1)
 
-        # [batch_size, n_subwords]
+        # [batch_size, seq_len]
         bert_lens = mask.sum(-1)
         bert_lens = bert_lens.masked_fill_(bert_lens.eq(0), 1)
         # [batch_size, seq_len, fix_len, hidden_size]
-        embed = bert.new_zeros(*mask.shape, self.hidden_size)
-        embed = embed.masked_scatter_(mask.unsqueeze(-1), bert[bert_mask])
+        embed = bert.new_zeros(*mask.shape, self.hidden_size).masked_scatter_(mask.unsqueeze(-1), bert[bert_mask])
         # [batch_size, seq_len, hidden_size]
-        embed = embed.sum(2) / bert_lens.unsqueeze(-1)
+        if self.pooling == 'first':
+            embed = embed[:, :, 0]
+        elif self.pooling == 'last':
+            embed = embed.gather(2, (bert_lens-1).unsqueeze(-1).repeat(1, 1, self.hidden_size).unsqueeze(2)).squeeze(2)
+        else:
+            embed = embed.sum(2) / bert_lens.unsqueeze(-1)
         embed = self.projection(embed)
 
         return embed
