81c5b8449bdfe031346a05222315e74334b6aca1
yzhangcs <yzhang.cs@outlook.com>
2021-04-27 01:02:11 +0800

    Test multilingual models

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import supar
 from supar import Parser
 
 
 def test_parse():
-    sentence = ['The', 'dog', 'chases', 'the', 'cat', '.']
-    for name in supar.PRETRAINED:
-        parser = Parser.load(name)
-        parser.predict(sentence, prob=True)
-        parser.predict(' '.join(sentence), prob=True, lang=('zh' if name.endswith('zh') else 'en'))
+    sents = {'en': 'She enjoys playing tennis.',
+             'zh': '她喜欢打网球.',
+             'de': 'Sie spielt gerne Tennis.',
+             'fr': 'Elle aime jouer au tennis.',
+             'ru': 'Она любит играть в теннис.',
+             'he': 'היא נהנית לשחק טניס.'}
+    tokenized_sents = {'en': ['She', 'enjoys', 'playing', 'tennis', '.'],
+                       'zh': ['她', '喜欢', '打', '网球', '.'],
+                       'de': ['Sie', 'spielt', 'gerne', 'Tennis', '.'],
+                       'fr': ['Elle', 'aime', 'jouer', 'au', 'tennis', '.'],
+                       'ru': ['Она', 'любит', 'играть', 'в', 'теннис', '.'],
+                       'he': ['היא', 'נהנית', 'לשחק', 'טניס', '.']}
+    for name, model in supar.NAME.items():
+        if 'xlmr' in name or 'roberta' in name or 'electra' in name:
+            continue
+        parser = Parser.load(name, reload=True)
+        if name.endswith(('en', 'zh')):
+            lang = name[-2:]
+            parser.predict(sents[lang], prob=True, lang=lang)
+            parser.predict(tokenized_sents[lang], prob=True, lang=None)
+        else:
+            for lang in sents:
+                parser.predict(sents[lang], prob=True, lang=lang)
+            parser.predict(list(tokenized_sents.values()), prob=True, lang=None)
+        os.remove(os.path.join(os.path.expanduser('~/.cache/supar'), model))
