eae3154faedfdda2e2f34b8f6d7c56d8bc392607
yzhangcs <yzhang.cs@outlook.com>
2021-04-28 22:50:51 +0800

    Add comments

--- supar/parsers/parser.py
+++ supar/parsers/parser.py
@@ -154,6 +154,30 @@ class Parser(object):
 
     @classmethod
     def load(cls, path, reload=False, src=None, **kwargs):
+        r"""
+        Loads a parser with data fields and pretrained model parameters.
+
+        Args:
+            path (str):
+                - a string with the shortcut name of a pretrained model defined in ``supar.MODEL``
+                  to load from cache or download, e.g., ``'biaffine-dep-en'``.
+                - a local path to a pretrained model, e.g., ``./<path>/model``.
+            reload (bool):
+                Whether to discard the existing cache and force a fresh download. Default: ``False``.
+            src (str):
+                Specifies where to download the model.
+                ``'github'``: github release page.
+                ``'hlt'``: hlt homepage, only accessible from 9:00 to 18:00 (UTC+8).
+                Default: None.
+            kwargs (dict):
+                A dict holding unconsumed arguments for updating training configs and initializing the model.
+
+        Examples:
+            >>> from supar import Parser
+            >>> parser = Parser.load('biaffine-dep-en')
+            >>> parser = Parser.load('./ptb.biaffine.dep.lstm.char')
+        """
+
         args = Config(**locals())
         args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         if src is not None:
