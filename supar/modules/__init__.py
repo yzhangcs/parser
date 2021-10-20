c372b39be98d4647e45dab6621d0c7021fc200ed
yzhangcs <yzhang.cs@outlook.com>
2021-10-20 16:33:46 +0800

    Transformer w/ relative pos

--- supar/modules/__init__.py
+++ supar/modules/__init__.py
@@ -6,6 +6,8 @@ from .lstm import CharLSTM, VariationalLSTM
 from .mlp import MLP
 from .pretrained import ELMoEmbedding, TransformerEmbedding
 from .scalar_mix import ScalarMix
+from .transformer import RelativePositionTransformerEncoder, TransformerEncoder
 
-__all__ = ['MLP', 'TransformerEmbedding', 'Biaffine', 'CharLSTM', 'ELMoEmbedding',
-           'IndependentDropout', 'ScalarMix', 'SharedDropout', 'Triaffine', 'VariationalLSTM']
+__all__ = ['MLP', 'TransformerEmbedding', 'Biaffine', 'CharLSTM', 'ELMoEmbedding', 'IndependentDropout',
+           'RelativePositionTransformerEncoder', 'ScalarMix', 'SharedDropout', 'TransformerEncoder', 'Triaffine',
+           'VariationalLSTM']
