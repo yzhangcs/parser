42688d0a79ec79fce436c04a165a9439a7c0ef9d
yzhangcs <yzhang.cs@outlook.com>
2023-05-11 15:34:05 +0800

    Deprecate `apply_permutation`

--- supar/modules/lstm.py
+++ supar/modules/lstm.py
@@ -7,7 +7,6 @@ from typing import List, Optional, Tuple
 import torch
 import torch.nn as nn
 from supar.modules.dropout import SharedDropout
-from torch.nn.modules.rnn import apply_permutation
 from torch.nn.utils.rnn import PackedSequence, pack_padded_sequence
 
 
@@ -170,10 +169,7 @@ class VariationalLSTM(nn.Module):
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         if permutation is None:
             return hx
-        h = apply_permutation(hx[0], permutation)
-        c = apply_permutation(hx[1], permutation)
-
-        return h, c
+        return hx[0].index_select(1, permutation), hx[1].index_select(1, permutation)
 
     def layer_forward(
         self,
